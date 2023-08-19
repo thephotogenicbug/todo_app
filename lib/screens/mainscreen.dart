fa39b3379aa3054bf37e42a79ec5b42d985f24eb
thephotogenicbug <navinkumarachari@gmail.com>
2023-08-19 21:06:57 +0530

    Setup TodoList app

---
+++ lib/screens/mainscreen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/screens/todo_list.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: TodoListPage(),
+    );
+  }
+}
---
+++ lib/screens/todo_list.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TodoListPage extends StatefulWidget {
+  const TodoListPage({super.key});
+
+  @override
+  State<TodoListPage> createState() => _TodoListPageState();
+}
+
+class _TodoListPageState extends State<TodoListPage>{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold();
+  }
+}
