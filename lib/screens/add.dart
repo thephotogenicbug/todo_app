d80d673202aa4a191cb9ed78e8571ef2f02d7c15
thephotogenicbug <navinkumarachari@gmail.com>
2023-08-19 21:52:25 +0530

    add background color

--- lib/screens/add.dart
+++ lib/screens/add.dart
@@ -68,12 +68,24 @@ class _AddPageState extends State<AddPage> {
   }
 
   void showSuccessMessage(String message) {
-    final snackBar = SnackBar(content: Text(message));
+    final snackBar = SnackBar(
+      content: Text(
+        message,
+        style: const TextStyle(color: Colors.white),
+      ),
+      backgroundColor: Colors.green,
+    );
     ScaffoldMessenger.of(context).showSnackBar(snackBar);
   }
 
   void showErrorMessage(String message) {
-    final snackBar = SnackBar(content: Text(message));
+    final snackBar = SnackBar(
+      content: Text(
+        message,
+        style: const TextStyle(color: Colors.white),
+      ),
+      backgroundColor: Colors.red,
+    );
     ScaffoldMessenger.of(context).showSnackBar(snackBar);
   }
 }
